b736ff0f39ee70617dcc1767901859104cad55db
hermian <hermian@gmail.com>
2025-08-02 15:34:40 +0900

    fix 파일복사

--- setup.py
+++ setup.py
@@ -1,10 +1,12 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 from Cython.Build import cythonize
 import numpy
 
 setup(
-        name='zigzag',
-        ext_modules=cythonize("zigzag/core.pyx"),
-        zip_safe=False,
-        include_dirs=[numpy.get_include()]
-        )
+    name='zigzag',
+    packages=find_packages(),  # zigzag 하위 디렉토리를 모두 패키지로 인식해 줌
+    ext_modules=cythonize("zigzag/core.pyx"),
+    include_dirs=[numpy.get_include()],
+    include_package_data=True,  # MANIFEST.in 등에서 지정한 추가 파일 포함
+    zip_safe=False,
+)
